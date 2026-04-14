87d8e7ce35b272734bbb8276a0c94f57a7bba41b
Georgi <gosheto420@gmail.com>
2026-04-14 13:24:09 +0300

    add Current Users

--- src/components/views/LiveChat/LiveChatPage.tsx
+++ src/components/views/LiveChat/LiveChatPage.tsx
@@ -19,6 +19,12 @@ type Message = {
   message: string;
   userName: string;
 };
+
+type ConnectionUser = {
+  userName: string;
+  id: string;
+};
+
 type MessageForm = Omit<Message, "userName">;
 
 const socket = io("http://localhost:8081");
@@ -26,12 +32,16 @@ export const LiveChatPage = () => {
   const [receiveMessage, setReceiveMessage] = useState<Message[]>([]);
   const { currentUser, isCheckCompleted } = useUserContext();
   const [snackMessage, setSnackMessage] = useState<string>();
+  const [listCurrentUser, setListCurrentUser] = useState<string[]>();
 
   const { handleSubmit, control } = useForm<MessageForm>();
 
   useEffect(() => {
     if (isCheckCompleted) {
-      socket.emit("JoinedUser", { userName: currentUser?.userName });
+      socket.emit("JoinedUser", {
+        userName: currentUser?.userName,
+        id: currentUser?.id,
+      } as ConnectionUser);
     }
   }, [isCheckCompleted]);
   const onSubmit: SubmitHandler<MessageForm> = async (data) => {
@@ -47,30 +57,57 @@ export const LiveChatPage = () => {
       setReceiveMessage([...receiveMessage, data]);
     });
 
-    socket.on("JoinedUser", (data) => {
+    socket.on("JoinedUser", (data: ConnectionUser) => {
       setSnackMessage(data.userName + " joined");
     });
 
-    socket.on("LeftUser", (data) => {
+    socket.on("LeftUser", (data: ConnectionUser) => {
       setSnackMessage(data.userName + " left");
+    });
+
+    socket.on("AllUsers", (data: string) => {
+      setListCurrentUser(data.split(","));
     });
   }, [receiveMessage]);
 
   useEffect(() => {
     return () => {
-      socket.emit("LeftUser", { userName: currentUser?.userName });
+      socket.emit("LeftUser", {
+        userName: currentUser?.userName,
+        id: currentUser?.id,
+      } as ConnectionUser);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const colorUserName = (userName: string) => {
+    const coleAvatar =
+      "#" +
+      (userName.charCodeAt(0) + userName.charCodeAt(1)) +
+      (userName.charCodeAt(2) + userName.charCodeAt(3));
+
+    return coleAvatar;
+  };
   return (
     <>
       <Box sx={{ mb: 1 }}>
+        <Box>
+          Current Users:
+          <Box sx={{ display: "flex" }}>
+            {listCurrentUser?.map((x) => {
+              const coleAvatar = colorUserName(x);
+
+              return (
+                <Box sx={{ bgcolor: coleAvatar, ml: 1, borderRadius: 1 }}>
+                  {x}
+                </Box>
+              );
+            })}
+          </Box>
+        </Box>
         <Typography>View Receive messages: </Typography>
         {receiveMessage.map((x) => {
-          const coleAvatar =
-            "#" +
-            (x.userName.charCodeAt(0) + x.userName.charCodeAt(1)) +
-            (x.userName.charCodeAt(2) + x.userName.charCodeAt(3));
+          const coleAvatar = colorUserName(x.userName);
           return (
             <Box sx={{ mt: 1, mb: 1, display: "flex" }}>
               <Tooltip title={x.userName}>
@@ -96,22 +133,20 @@ export const LiveChatPage = () => {
           control={control}
           name="message"
           render={({ field: { onChange, value } }) => (
-            <TextField
-              value={value}
-              label="Message"
-              fullWidth
-              onChange={onChange}
-            />
+            <Box sx={{ display: "flex", width: "50%" }}>
+              <TextField
+                value={value}
+                label="Message"
+                fullWidth
+                multiline
+                onChange={onChange}
+              />
+              <Button type="submit" variant="contained" endIcon={<SendIcon />}>
+                send
+              </Button>
+            </Box>
           )}
         />
-        <Button
-          type="submit"
-          sx={{ mt: 1 }}
-          variant="contained"
-          endIcon={<SendIcon />}
-        >
-          send
-        </Button>
       </Form>
 
       <Snackbar
